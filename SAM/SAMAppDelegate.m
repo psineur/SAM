c167a15bec0e8676faad6f55d7a6935f721a7ced
Stepan Generalov <psi.pk.ru@gmail.com>
2013-03-15 22:23:42 -0700

    Add refresh menu item to fetch workspaces from asana.

--- SAM/SAMAppDelegate.m
+++ SAM/SAMAppDelegate.m
@@ -8,11 +8,13 @@
 
 #import "SAMAppDelegate.h"
 #import "SAMSettingsViewController.h"
+#import <AFNetworking.h>
 
 @interface SAMAppDelegate ()
 
 @property (strong, nonatomic) SAMSettingsViewController *settings;
 @property (strong, nonatomic) NSWindow *settingsWindow;
+@property (strong, nonatomic) AFHTTPClient *client;
 
 @end
 
@@ -20,6 +22,11 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    self.settings = [[SAMSettingsViewController alloc] init];
+    self.client = [AFHTTPClient clientWithBaseURL: [NSURL URLWithString:@"https://app.asana.com/api/1.0/"]];
+    [self.client setAuthorizationHeaderWithUsername: self.settings.APIToken password:@""];
+
+    NSLog(@"API token = %@", self.settings.APIToken);
 }
 
 - (IBAction) fullscreenPressed: (id) sender
@@ -28,14 +35,26 @@
 }
 
 - (IBAction)settingsPressed:(id)sender
-{
-    if (!self.settings)
-        self.settings = [[SAMSettingsViewController alloc] init];
-    
+{   
     self.settingsWindow =
     [[NSWindow alloc] initWithContentRect: self.settings.view.frame styleMask: NSTitledWindowMask backing: NSBackingStoreBuffered defer: YES];
     _settingsWindow.contentView = self.settings.view;
     [_settingsWindow makeKeyAndOrderFront: self];
+}
+
+- (IBAction)refresh:(id)sender
+{
+    [self.client setAuthorizationHeaderWithUsername: self.settings.APIToken password:@""];
+    NSURLRequest *request = [self.client requestWithMethod:@"GET" path:@"workspaces" parameters: @{}];
+    AFJSONRequestOperation *test =
+    [AFJSONRequestOperation JSONRequestOperationWithRequest: request
+                                                    success: ^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+                                                        NSLog(@"result = %@", JSON);
+                                                    }
+                                                    failure: ^(NSURLRequest *request, NSHTTPURLResponse *response, id error, id JSON) {
+                                                        NSLog(@"result = %@", JSON);
+                                                    }];
+    [test start];
 }
 
 @end
