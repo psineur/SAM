7e40eba88ceb95c35329a752ccf4b2aa0cb73b13
Stepan Generalov <psi.pk.ru@gmail.com>
2013-03-17 18:27:02 -0700

    Add SAMDefines.h to project

---
+++ SAM/SAMDefines.h
@@ -0,0 +1,15 @@
+//
+//  SAMDefines.h
+//  SAM
+//
+//  Created by Stepan Generalov on 17.03.13.
+//  Copyright (c) 2013 Stepan Generalov. All rights reserved.
+//
+
+#ifndef SAM_SAMDefines_h
+#define SAM_SAMDefines_h
+
+#define SELFTYPE __typeof(&*self)
+
+
+#endif
